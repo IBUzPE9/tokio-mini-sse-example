4a5c69d87bf67c2c06cb6e716899c35ff66283f5
Alex <alex@drop.it>
2017-06-29 18:21:23 +0300

    v0.1.2

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#A simple server sent events example 
+
+Written in Rust using tokio-minihttp crate.
+
+Usage:
+    Just start the server and open the link: http://127.0.0.1:7778/
--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,14 @@ use std::io;
 use std::io::Write;
 use std::time::Duration;
 
-// there is no way to create response without `Content-Length` header with tokio-minihttp, so let's do a little trick
-static SSE_RESP:&[u8] = b"HTTP/1.1 200 OK\r\nContent-Type: text/event-stream\r\nConnection: keep-alive\r\nAccess-Control-Allow-Origin: *\r\n\r\n";
+enum Case<A,B,C,D>{A(A), B(B), C(C), D(D)}
 
-static LOTR_CHARS:&[&str] = &["Frodo", "Bilbo", "Gimli", "Legolas", "Gandalf", "Sam", "Gollum", "Aragorn", "Boromir", "Galadriel"];
-static LOTR_DEEDS:&[&str] = &["wounded by King Nazgul’s blade", "leaves the Ring behind", "is fascinated by Galadriel's beauty", "hits a flying Nazgul", "overthrows Saruman and breaks his staff", "returned to the Shire", "wanders through the lands of Middle-earth", "ruled Gondor", "dies heroically", "is independent, strong and wise"];
-
-enum Case<A,B,C>{A(A), B(B), C(C)}
-
-impl<A,B,C,I,E> Future for Case<A,B,C>
+impl<A,B,C,D,I,E> Future for Case<A,B,C,D>
 where 
     A:Future<Item=I, Error=E>,
     B:Future<Item=I, Error=E>,
     C:Future<Item=I, Error=E>,
+    D:Future<Item=I, Error=E>,
 {
     type Item = I;
     type Error = E;
@@ -40,6 +35,7 @@ where
             Case::A(ref mut a) => a.poll(),
             Case::B(ref mut b) => b.poll(),
             Case::C(ref mut c) => c.poll(),
+            Case::D(ref mut d) => d.poll(),
         }
     }
 }
@@ -62,40 +58,88 @@ fn main() {
             .and_then(|(req, transport)| 
                 match req {
                     Some(req) => match req.path() {
-                        "/" => Case::A(
-                            write_all(transport.into_inner(), SSE_RESP)
-                                .and_then(|(stream,_)|
-                                    loop_fn((stream, 9, Vec::new()), |(stream, cnt, mut buf)|
-                                        Timeout::new(Duration::from_secs(2), rht).unwrap()
-                                            .and_then(move |_|{
-                                                let port = stream.peer_addr().map(|addr| addr.port()).unwrap_or(42) as usize;
-                                                let char = rnd(cnt + port);
-                                                let deed = rnd(char + port);
-                                                buf.clear();
-                                                write!(buf, "event: userconnect\ndata: {{\"username\": \"{}\", \"status\": \"{}\"}}\n\n", LOTR_CHARS[char], LOTR_DEEDS[deed]).unwrap();
-                                                write_all(stream, buf)
-                                                    .and_then(move |(stream, buf)|
-                                                        if cnt != 0 {
-                                                            Ok(Loop::Continue((stream, cnt-1, buf)))
-                                                        }else{
-                                                            Ok(Loop::Break(()))
-                                                        }                                                    
-                                                    )                                                    
-                                            })  
+                        "/events" => {
+                            println!("request events");
+                            Case::A(
+                                write_all(transport.into_inner(), SSE_RESP)
+                                    .and_then(|(stream,_)|
+                                        loop_fn((stream, 9, Vec::new()), |(stream, cnt, mut buf)|
+                                            Timeout::new(Duration::from_secs(2), rht).unwrap()
+                                                .and_then(move |_|{
+                                                    let port = stream.peer_addr().map(|addr| addr.port()).unwrap_or(42) as usize;
+                                                    let char = rnd(cnt + port);
+                                                    let deed = rnd(char + port);
+                                                    buf.clear();
+                                                    write!(buf, "event: userconnect\ndata: {{\"username\": \"{}\", \"status\": \"{}\"}}\n\n", LOTR_CHARS[char], LOTR_DEEDS[deed]).unwrap();
+                                                    write_all(stream, buf)
+                                                        .and_then(move |(stream, buf)|
+                                                            if cnt != 0 {
+                                                                Ok(Loop::Continue((stream, cnt-1, buf)))
+                                                            }else{
+                                                                Ok(Loop::Break(()))
+                                                            }                                                    
+                                                        )                                                    
+                                                })  
 
+                                        )
                                     )
-                                )
-                        ),
+                                    .or_else(|e| {println!("{:?}", e); Ok(())})
+                            )
+                        }
+                        "/" => {
+                            println!("request html");
+                            let mut resp = Response::new();
+                            resp.status_code(200, "Ok");
+                            resp.body(HTML_PAGE);
+                            Case::B(transport.send(resp).map(|_| ())
+                                .or_else(|e| {println!("{:?}", e); Ok(())})
+                            )                            
+                        }
                         _ =>  {
+                            println!("request is wrong");
                             let mut resp = Response::new();
                             resp.status_code(404, "Not Found");
                             resp.body("not found");
-                            Case::B(transport.send(resp).map(|_| ()))                                 
+                            Case::C(transport.send(resp).map(|_| ())
+                                .or_else(|e| {println!("{:?}", e); Ok(())})
+                            )                                 
                         }
                     },
-                    _ => Case::C(future::err(io::Error::from(io::ErrorKind::InvalidInput)))
+                    _ => Case::D(future::err(io::Error::from(io::ErrorKind::InvalidInput)))
                 }
             )
     });
     core.run(server).unwrap();
 }
+
+
+static LOTR_CHARS:&[&str] = &["Frodo", "Bilbo", "Gimli", "Legolas", "Gandalf", "Sam", "Gollum", "Aragorn", "Boromir", "Galadriel"];
+static LOTR_DEEDS:&[&str] = &["wounded by King Nazgul’s blade", "leaves the Ring behind", "is fascinated by Galadriel's beauty", "hits a flying Nazgul", "overthrows Saruman and breaks his staff", "returned to the Shire", "wanders through the lands of Middle-earth", "ruled Gondor", "dies heroically", "is independent, strong and wise"];
+
+// there is no way to create response without `Content-Length` header with tokio-minihttp, so let's do a little trick
+static SSE_RESP:&[u8] = b"HTTP/1.1 200 OK\r\nContent-Type: text/event-stream\r\nConnection: keep-alive\r\nAccess-Control-Allow-Origin: *\r\n\r\n";
+
+static HTML_PAGE:&str = r#"<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"> 
+    <title>Rust tokio-minihttp example with SSE and hobbits</title>
+  </head>
+  <body>
+    <h1>Rust tokio-minihttp example with SSE and hobbits</h1>
+
+    <div id="sse-msg">
+    </div>
+
+    <script type="text/javascript">
+      var evtSource = new EventSource("http://127.0.0.1:7778/events");
+
+      evtSource.addEventListener("userconnect", function(e) {
+          var sseMsgDiv = document.getElementById('sse-msg');
+          const obj = JSON.parse(e.data);
+          sseMsgDiv.innerHTML += '<p>' + obj.username + " " + obj.status + '</p>';
+          console.log(obj);
+      }, false);
+    </script>
+  </body>
+</html>"#;
